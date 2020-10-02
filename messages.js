cf260dd84c07f200008357e142bda3cac31f0980
sgrogov <sg.rogov@yandex.ru>
2020-10-03 00:01:41 +0200

    Adjust messages

--- messages.js
+++ messages.js
@@ -9,9 +9,11 @@ Viel Spaß!`,
   cashFloatReport: (salespoints) => {
     if (salespoints.length === 0) return 'Пока никаких данных об остатке не получено';
     return `${salespoints.map(
-      ({ salespointId, amount, reportedAt }) => `${salespointId.padEnd(8, ' ')} ${amount.toString().padEnd(8, ' ')} ${reportedAt.toISOString()}`
-    ).join('\n')}`
+      ({ salespointId, amount, reportedAt }) => `Точка продаж: ${salespointId}
+Остаток: ${amount}
+Обновлено: ${reportedAt.toISOString()}`
+    ).join('\n\n')}`
   },
   somethingWentWrong: 'Что-то пошло не так :(',
-  adminUsernameMismatch: 'Извините, у вас нет полномочий использовать этого бота :(',
+  adminUsernameMismatch: 'У вас нет полномочий использовать этого бота :(',
 };
