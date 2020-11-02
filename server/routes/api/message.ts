dd2a1b958860697b00336b65bc6e573ece86770a
sgrogov <sg.rogov@yandex.ru>
2020-11-02 22:45:47 +0100

    Fix messages charset

--- server/routes/api/message.ts
+++ server/routes/api/message.ts
@@ -1,3 +1,4 @@
+import iconv from 'iconv-lite';
 import { AuthorizedRequest, getToken } from "../utils/auth/authorized";
 
 interface MessageRequest {
@@ -10,12 +11,31 @@ interface MessageResponse {
   send: (response: string) => void;
 }
 
+const containsCyrillic = (input: string) => /[А-я]/i.test(input);
+
+const fixCharset = (input: string) => {
+  if (!containsCyrillic(input)) {
+    console.log('Fixing charset...');
+    const buf = iconv.encode(input, 'win1252');
+    const output = iconv.decode(buf, 'utf8');
+
+    if (containsCyrillic(output)) {
+      console.log('Charset fixed');
+      return output;
+    }
+    console.log('Charset not fixed');
+  }
+  return input;
+};
+
 const createMessageRoute = (sendMessage: (token: string, message: string) => void) => (
   req: MessageRequest & AuthorizedRequest,
   res: MessageResponse,
 ) => {
   const token = getToken(req);
-  const text = req.query.text || req.body;
+  let text = req.query.text || req.body;
+
+  text = fixCharset(text);
 
   console.log('Sending message:', text);
   sendMessage(token, text);
